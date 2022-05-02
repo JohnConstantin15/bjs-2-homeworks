f4a526da7214fae7c0f34cc7f7129d36b4f22e51
JohnConstantin15 <johnconstantin15@gmail.com>
2022-05-02 07:02:10 +0300

    dz2 commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = sum / arr.length;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  let sum;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let arrParams = getArrayParams(arr);
+  let dif = Math.abs(arrParams.max - arrParams.min);
+  return dif;
 }
