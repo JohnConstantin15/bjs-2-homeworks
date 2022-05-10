7286789054d2b9b91712f7808a6e689051446c7c
JohnConstantin15 <johnconstantin15@gmail.com>
2022-05-11 01:14:18 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  if(arr1.length===arr2.length && arr1.every((value,index)=>value === arr2[index])){
+    result=true;
+  } 
+  else{
+    result=false;
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr=arr.filter(element => element>0).filter(element => (element % 3===0)).map(element=>element*10);
 
   return resultArr; // array
 }
